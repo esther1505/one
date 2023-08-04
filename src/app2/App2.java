120ee9c71fa8ff3b36a54e4186fbde862d22be27
antro ranjani <antroranjani2001@gmail.com>
2023-08-04 12:12:16 +0530

    app2 file

---
+++ src/app2/App2.java
@@ -0,0 +1,53 @@
+package app2;
+import java.util.Scanner;
+
+
+public class App2 {
+
+    
+    public static void main(String[] args) {
+        //operators, =,==,>,<,<=,>=,
+       /* int a=34;
+        int b=56;
+        System.out.println(a!=b);
+        System.out.println(a=b);
+        System.out.println(a<=b);
+        System.out.println(a==b);
+        System.out.println(a>b);
+        //Input and Output->Input meaning=get user Input
+        String text= "Welcome to IOB";
+        System.out.println(text);
+        //Scanner Input*/
+       
+        Scanner in=new Scanner(System.in);
+        /*System.out.println("Enter Your age:");
+        int age=in.nextInt();
+        System.out.println(age+2);*/
+        
+       
+       
+       /*int sum1=100+50;
+       int sum2=sum1+50;
+       int sum3=sum2+sum2;
+        System.out.println(sum1);
+        System.out.println(sum2);
+        System.out.println(sum3);*/
+       
+       int num1, num2;
+        System.out.println("Enter the sum1: ");
+        num1=in.nextInt();
+        System.out.println("Enter the sum2: ");
+        num2=in.nextInt();
+        System.out.println("num1/num2 Added:"+(num1/num2));
+        
+        
+        
+       
+       
+                
+        
+       
+        
+           }
+    
+}
